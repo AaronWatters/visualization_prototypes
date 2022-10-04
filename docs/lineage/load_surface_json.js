ec7f61d3284c99a7286b767f681709a9925a5510
Aaron Watters <awatters@simonsfoundation.org>
2022-10-04 11:42:05 -0400

    add examples

--- docs/lineage/load_surface_json.js
+++ docs/lineage/load_surface_json.js
@@ -1,18 +1,39 @@
 
 var DATA_FILE = "./lineage.json";
-var SIDE = 1000;
+
+var DATA_MAP = {
+    "F32Full.json": "F32Full",
+    "F32_ICMTE.json": "F32_ICMTE",
+}
+var SIDE = 800;
 var radius_multiple = 2;
 
-var info, surfaces;
+var info, surfaces, options, filename;
 var json_data = null;
 
 function load_surfaces() {
     info = $("#info");
     surfaces = $("#surfaces");
     surfaces.width(SIDE).height(SIDE);
-    surfaces.css("background-color", "cyan")
-    info.html("loading: " + DATA_FILE);
-    $.getJSON(DATA_FILE, setup).fail(on_load_failure);
+    surfaces.css("background-color", "cyan");
+    var sources = $("#sources");
+    sources.empty();
+    options = $("<select/>").appendTo(sources);
+    var selected = ""
+    for (var filename in DATA_MAP) {
+        var abbrev = DATA_MAP[filename];
+        $(`<option value="${filename}" ${selected}> ${abbrev} </option>`).appendTo(options);
+        selected = "";
+    }
+    load_selected_file();
+    //options.on("change", load_selected_file)
+    options.change(load_selected_file);
+}
+
+function load_selected_file() {
+    var filename = options.find(":selected").val();
+    info.html("loading: " + filename);
+    $.getJSON(filename, setup).fail(on_load_failure);
 };
 
 var on_load_failure = function() {
@@ -23,7 +44,7 @@ var on_load_failure = function() {
 var setup = function(data) {
     debugger;
     json_data = data
-    info.html("loaded!");
+    info.html("loaded " + filename);
     var container = surfaces;
     var display3d = container.surfaces_sequence(json_data);
     display3d.load_3d_display(container);
